6ab30deea0e7c5a84f4d45574b170c0b561bfb76
Lerishy <2230008711@qq.com>
2022-09-29 09:47:11 +0800

    配置了element-ui和axios

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    //端口号
+    port: 8888,
+    //主机名
+    host: "localhost",
+    //是否自动开启浏览器
+    open: true,
+    //是否开启https
+    https: false
+  }
 })
